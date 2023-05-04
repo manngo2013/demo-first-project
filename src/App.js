0b147fb73f8edb831d31f15fc6b24972ed9256b0
manngo <manngo2013@gmail.com>
2023-05-04 09:31:29 +0700

    Lecture 11: Rendering Elements

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
 
 function App() {
+  const username = 'Adam';
+  const age = 18;
+
+  const employee = {
+    username: 'John',
+    age: 20,
+  };
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>
+        Hello: {username} - {age}
+      </h1>
+      <br />
+      <h2>
+        Employe: {employee.username} - {employee.age}
+      </h2>
     </div>
   );
 }
