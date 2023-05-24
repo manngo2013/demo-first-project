4547eab41f35e79b89417b20f58a8237d89d5f1a
manngo <manngo2013@gmail.com>
2023-05-24 19:25:42 +0700

    Lecture 18: State

--- src/index.js
+++ src/index.js
@@ -6,15 +6,17 @@ import reportWebVitals from './reportWebVitals';
 import ClassComponent from './components/ClassComponent';
 import FunctionComponent from './components/FunctionComponent';
 import ProductDetailComponent from './components/ProductDetailComponent';
+import CounterComponent from './components/CounterComponent';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <ClassComponent title="Demo Using Prop in class component" />
-    <br />
+    {/* <ClassComponent title="Demo Using Prop in class component" />
+    <br /> */}
     {/* <FunctionComponent /> */}
-    <ProductDetailComponent title="Iphone 14" content="Description ..... " />
+    {/* <ProductDetailComponent title="Iphone 14" content="Description ..... " /> */}
+    <CounterComponent />
   </React.StrictMode>
 );
 
